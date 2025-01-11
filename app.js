f217628decf2cd1774573f368a6b9e966329abc1
pavithra05e <pavithra.e2023cce@sece.ac.in>
2025-01-11 12:33:16 +0530

    after atlas connection

--- app.js
+++ app.js
@@ -1,80 +1,97 @@
-var express = require('express')
-const mongoose=require("mongoose")
-const {v4: uuidv4 } = require("uuid")  // import uuid
-const cors=require('cors')
-// app creation
+var express=require("express");
+const mongoose=require("mongoose");
+const {v4:uuidv4}=require("uuid")
+
 const app=express();
+app.use(express.json());
+const cors = require("cors")
 app.use(cors())
-// middleware
-app.use(express.json())
-mongoose.connect("mongodb+srv://pavithra:pavisecemern@cluster0.xezm2.mongodb.net/").then(()=>{
+mongoose.connect(
+   "mongodb+srv://pavithra:pavisecemern@cluster0.xezm2.mongodb.net/"
+)
+.then(()=>{
     console.log("Connected to DB")
 })
 
+// const students=[{
+//     id:1,
+//     name:"Jaxson",
+//     age:19
+// },{
+//     id:2,
+//     name:"Grace",
+//     age:19
+// }]
+//connecting db
+// mongoose.connect("mongodb://localhost:27017/expense")
+//     .then(()=>{console.log("Connected")});
+//returns a promise(resolve or reject)
+
+//creating schema
 const expenseSchema=new mongoose.Schema({
-    id: { type: String, required:true, unique: true}, 
-    title:{type: String, required: true},
-    amount: {type:Number, required: true}
-})
+    id:{type:String,required:true,unique:true},
+    title:{type:String,required:true},
+    amount:{type:Number,required:true}
+});
 
-const Expenses=mongoose.model("Expenses", expenseSchema);
+//model creation
+const Expenses=mongoose.model("Expenses",expenseSchema)
 
-app.get("/api/expenses", async(req,res)=> {
+app.get("/api/expenses",async(req,res)=>{
     try{
-    const expenses= await Expenses.find()
-    res.status(200).json({expenses})
-    } catch(err){
-        res.status(500).json(err.message)
+    const expenses=await Expenses.find();
+    res.status(200).json(expenses)    }
+    catch(err){
+        res.status(500).json({message:err.message})
     }
 })
 
-app.get("/api/expenses/:id", async(req,res)=> {
+app.get("/api/expenses/:id",async(req,res)=>{
     try{
-        const {id} = req.params;
-        const expense=await Expenses.findOne({id})
+        const {id}=req.params;
+        const expense=await Expenses.findOne({id});
         if(!expense){
-            return res.status(404).json({message: "Expense not found"})
+            return res.status(404).json({message:"Expense not found"})
         }
         res.status(200).json(expense)
-    } catch(error){
-        res.status(500).json(error.message)
     }
-})
-
-app.post("/api/expenses", async(req,res)=> {
-    console.log(req.body)
-    const{ title, amount}=req.body
-    try{
-        const newExpense = new Expenses({
-            id: uuidv4(),
-            title,  // this is same as title: title
-            amount
-        })
-        const savedExpense = await newExpense.save()
-        res.status(200).json(savedExpense)
-    }  
     catch(err){
-        res.status(500).json({message: "Error in creating expense"})
+        res.status(500).json({message:"Error in fetching"})
     }
-})
+});
 
-app.put("/api/expenses/:id", async(req, res) => {
-    const {id}=req.params;
-    const {title, amount}= req.body;
+app.post("/api/expenses",async(req,res)=>{
+    console.log(req.body);
+    const{title,amount}=req.body;
     try{
-        const updateExpense = await Expenses.findOneAndUpdate(
-            {id},
-            {title, amount}
-        )
-        if(!updateExpense){
-           return res.status(404).json({message: "Expense not found"})
+    const newExpense=new Expenses({//object in the model of given model
+        id:uuidv4(),
+        title,
+        amount
+    });
+    const saveExpense=await newExpense.save()
+    res.status(200).json(newExpense);
+    }
+    catch(err){
+        res.status(500).json({message:"POST unsuccessful"})}
+}
+)
+
+app.put("/api/expenses/:id",async(req,res)=>{
+        const {id}=req.params;
+        const {title,amount}=req.body;
+        try{
+            const updateexpense=await Expenses.findOneAndUpdate({id},{title,amount})//new true creates a new data if the the id doesnt exist
+        if(!updateexpense){
+            return res.status(404).json({message:"Expense not found"})
         }
-        res.status(200).json(updateExpense)
+        res.status(200).json(updateexpense)
     }
-    catch(err){
-        res.status(500).json({message: "Error in updating expense"})
-    }
-})
+        catch(err){
+            res.status(500).json({message:"Put failed"})
+        }
+
+});
 
 app.delete("/api/expenses/:id",async(req,res)=>{
     const {id}=req.params;
@@ -87,36 +104,29 @@ app.delete("/api/expenses/:id",async(req,res)=>{
     }
     catch(err){
         res.status(500).json({message:err.message})
-    }
+    }
 })
-app.listen(3000,()=>{
-    console.log("Server is running on port 3000")
-})
-/*
-const students=[{
-    name:"hari",
-    age:21
-},{
-    name:"kavi",
-    age:23
-}]
+// app.get("/api/sayhello",(req,res)=>{
+//     res.send("Hello, World!");                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
+//     res.end();
+//     }) 
 
-//read data
-app.get("/api/sayhello",(req,res)=>{
-    res.send("Hello cce!!")
-    res.end();
+// app.get("/api/students",(req,res)=>{
+//     res.status(200).json(students)
+// })
+
+// app.get("/api/students/:id",(req,res)=>{
+//     const {id}=req.params;
+//     const student=students.find((student)=>student.id==id);
+//     if(!student){
+//         res.status(404).json({message:"student not found"});
+//     }
+//     else{
+//         res.status(200).json(student);
+//         console.log(student);
+//     }
+// })
+    
+app.listen(3000,()=>{
+    console.log("Server is running on Port 3000")
 })
-app.get("/api/students",(req,res)=>{
-    res.status(200).json(students);
-})
-app.get("/api/students/:age",(req,res)=>{
-    const {age}=req.params;
-    const student=students.find((student)=>student.age==age);
-    if(!student){
-        res.status(404).json({message:"Student not found"})
-    }
-    else{
-        res.status(200).json(student);
-    }
-})
-*/
